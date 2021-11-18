c38902b2503a04ade4bd6e93545bdccd5b018376
dhaydt <store>
2021-11-18 21:16:24 +0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,8 @@ npm run build
 # git add -A
 # git commit -m 'deploy'
 
-git add . && git commit -m "deploys"                                     ─╯
+git add .
+git commit -m "deploys"                                     ─╯
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:dhaydt/vue-porto.git origin:gh-pages
