b907ab5fd47ffe80b1633319890941fbe16b9750
dhaydt <store>
2021-11-18 21:10:15 +0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env sh
+
 # abort on errors
 set -e
+
 # build
 npm run build
+
 # navigate into the build output directory
 # cd dist
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-git init
+
+# git init
 # git add -A
-git add dist && git commit -m "autodeploy"
 # git commit -m 'deploy'
-# git push -f git@github.com:dhaydt/vue-porto.git gh-pages
-git subtree push -f --prefix dist origin gh-pages 
+
+git add dist && git commit -m "deploys"                                     ─╯
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:dhaydt/vue-porto.git origin:gh-pages
+
+git subtree push --prefix dist origin gh-pages  
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+
 # cd -
